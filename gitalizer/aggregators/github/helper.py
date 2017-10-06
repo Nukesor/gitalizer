bae0c1f135bcdea159ed7031e33ffa6f1a27d982
Arne Beer <arne@twobeer.de>
2017-10-06 23:04:15 +0200

    Try in contributer to avoid endless loop

--- gitalizer/aggregators/github/helper.py
+++ gitalizer/aggregators/github/helper.py
@@ -4,12 +4,15 @@ import socket
 
 def get_commit_count(contributors):
     """Return the amount of commits in a repository by their contributors."""
-    received = False
-    while not received:
+    _try = 0
+    tries = 3
+    while _try <= tries:
         try:
             count = 0
             for contributer in contributors:
                 count += contributer.contributions
             return count
         except socket.timeout:
+            print(f'Failed to receive contributors. Try {_try} of {tries}')
             pass
+    raise Exception
