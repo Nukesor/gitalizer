095a071f2ae4e24788763b5044f452d920e1d22a
Arne Beer <arne@twobeer.de>
2017-12-10 17:36:26 +0100

    Plot user repository changes

--- gitalizer/plot/helper.py
+++ gitalizer/plot/helper.py
@@ -1,4 +1,5 @@
 """Helper for getting specific data sets."""
+import matplotlib.pyplot as plt
 
 from gitalizer.extensions import db
 from gitalizer.models.repository import Repository
@@ -16,3 +17,14 @@ def get_user_repositories(contributer):
         .all()
 
     return repositories
+
+
+def plot_figure(path, ax):
+    """Save a plot to a graph."""
+    plt.xticks(rotation=30)
+    plt.figure(figsize=(20, 10))
+
+    fig = ax.get_figure()
+    fig.savefig(path)
+
+    plt.close(fig)
--- gitalizer/plot/user.py
+++ gitalizer/plot/user.py
@@ -3,11 +3,14 @@
 import os
 import math
 import pandas as pd
+import seaborn as sns
+from matplotlib.ticker import FixedFormatter
+
 from gitalizer.extensions import db
 from gitalizer.models.email import Email
 from gitalizer.models.commit import Commit
 from gitalizer.models.contributer import Contributer
-from gitalizer.plot.helper import get_user_repositories
+from gitalizer.plot.helper import get_user_repositories, plot_figure
 
 
 def plot_user_repositories_changes(name, path):
@@ -33,35 +36,55 @@ def plot_user_repository_changes(contributer, repo, path):
     """Plot the changes for a specific contributer and repository."""
     commits = db.session.query(Commit) \
         .filter(Commit.repository == repo) \
-        .join(Email, Email.email == Commit.author_email) \
+        .join(Email, Email.email == Commit.author_email_address) \
         .join(Contributer, Email.contributer_login == Contributer.login) \
         .filter(Contributer.login == contributer.login) \
         .all()
+
     if len(commits) < 20:
         return
 
     data = []
     for c in commits:
-        time = c.time.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-        if c.additions and c.deletions:
-            data.append({
-                'date': time,
-                'additions': c.additions,
-                'deletions': c.deletions,
-                'changes': math.fabs(c.additions - c.deletions),
-            })
+        if not c.additions or not c.deletions:
+            continue
+        time = c.time.replace(
+            day=1, hour=0, minute=0,
+            second=0, microsecond=0,
+            tzinfo=None,
+        )
+        data.append({
+            'date': time,
+            'additions': c.additions,
+            'deletions': c.deletions,
+            'changes': math.fabs(c.additions - c.deletions),
+        })
 
+    # Basic dataframe by date (month)
     df = pd.DataFrame(data=data)
     df = df.sort_values(by='date')
-    df = df.set_index('date')
+    df = df.set_index('date', drop=True)
 
     color = dict(boxes='DarkGreen', whiskers='DarkOrange',
                  medians='DarkBlue', caps='Gray')
 
-    box = df.plot.box(color, sym='r+')
-    fig = box.get_figure()
+    print(repo.name)
+    # Format dataframe for plotting
+    df_stacked = df.stack()
+    print(df_stacked)
+    df_stacked = df_stacked.reset_index()
+    df_stacked.columns = ["date", "vars", "vals"]
+    df_stacked.set_index('date', drop=True, inplace=True)
+
+    # Create box plot
+    box = sns.boxplot(x=df_stacked.index, y="vals", hue="vars", data=df_stacked)
+
+    # Format dates
+    dates = df_stacked.index.unique().to_series()
+    dates = dates.dt.strftime("%Y-%m")
+    box.xaxis.set_major_formatter(FixedFormatter(dates))
 
     plot_path = os.path.join(path, repo.name)
-    fig.savefig(plot_path)
+    plot_figure(plot_path, box)
 
-    print(df)
+    return
