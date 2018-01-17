854b445aa2004e41ef58b56ebfb569eca1668529
Arne Beer <arne@twobeer.de>
2018-01-17 22:43:24 +0100

    Better syncdb script

--- bin/syncdb.sh
+++ bin/syncdb.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/bash
 
-ssh nuke@jarvis 'pg_dump gitalizer > gitalizer.dump'
-rsync nuke@jarvis:gitalizer.dump ./
+echo 'Dumping DB on remote'
+ssh nuke@jarvis 'pg_dump -F c gitalizer > gitalizer.dump'
+echo 'Sync DB'
+scp nuke@jarvis:gitalizer.dump ./
+
+echo 'Drop and recreate DB'
 dropdb gitalizer || true
 createdb gitalizer
-psql gitalizer < gitalizer.dump
+
+echo 'Restoring DB'
+pg_restore -j 4 -F c -d gitalizer gitalizer.dump
+
+echo 'Deleting dumps'
 rm gitalizer.dump
 ssh nuke@jarvis 'rm gitalizer.dump'
+echo 'Done'
