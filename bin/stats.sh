c6bbb2e778bcde06c75144f1f9c362e2ef88c387
Arne Beer <arne@twobeer.de>
2018-01-24 13:18:51 +0000

    Specify db in stats

--- bin/stats.sh
+++ bin/stats.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+db=gitalizer
 
-result=$(psql -d gitalizer -t -c "SELECT pg_size_pretty(pg_database_size('gitalizer'));")
+result=$(psql -d $db -t -c "SELECT pg_size_pretty(pg_database_size('gitalizer'));")
 echo "DB size: $result"
 
-result=$(psql -d gitalizer -t -c 'SELECT count(*) FROM commit;')
+result=$(psql -d $db -t -c 'SELECT count(*) FROM commit;')
 echo "commit count: $result"
 
-result=$(psql -d gitalizer -t -c 'SELECT count(*) FROM repository;')
+result=$(psql -d $db -t -c 'SELECT count(*) FROM repository;')
 echo "repository count: $result"
 
-result=$(psql -d gitalizer -t -c 'SELECT count(*) FROM repository WHERE completely_scanned = TRUE OR broken = TRUE OR fork = TRUE;')
+result=$(psql -d $db -t -c 'SELECT count(*) FROM repository WHERE completely_scanned = TRUE OR broken = TRUE OR fork = TRUE;')
 echo "Finished or filtered repository count: $result"
 
-result=$(psql -d gitalizer -t -c 'SELECT count(*) FROM contributer;')
+result=$(psql -d $db -t -c 'SELECT count(*) FROM contributer;')
 echo "contributer count: $result"
